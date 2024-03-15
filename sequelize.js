1d6e5604cb5cbf81fcefe14fc1780b829612717c
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-03-15 14:12:38 -0500

    postgres login issues

--- sequelize.js
+++ sequelize.js
@@ -1,7 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+// Read the DATABASE_URL environment variable
+const databaseUrl = process.env.DATABASE_URL;
+
+// Initialize Sequelize instance with DATABASE_URL
+const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres'
 });
 
+// Export the Sequelize instance
 module.exports = sequelize;
